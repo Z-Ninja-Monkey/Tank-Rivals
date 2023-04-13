561d9437c25d58007b3dbcd111f7a7b4e38cdfe3
Z-Ninja-Monkey <ansonkiehl@gmail.com>
2023-04-12 17:46:34 -0700

    gamedev

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let sprite;
+
+function preload(){
+	//image = loadImage('assets/blank.png');
+}
+
+function setup() {
+	let canvas = createCanvas(1000, 590);
+	canvas.parent("Game");
+	frameRate(60);
+	
+}
+
+function draw() {
+	clear();
+	background('white');
+}
